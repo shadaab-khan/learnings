0907212a7d797fed4c503a22dd96acc2b1745034
shadaab-khan <shadaab@harvestglobal.com>
2019-07-29 23:40:02 +0530

    Created HashMapClass added recursion in Random Numbers

---
+++ src/skills/MyHashMap.java
@@ -0,0 +1,8 @@
+package skills;
+
+import java.util.HashMap;
+
+public class MyHashMap extends HashMap {
+
+
+}
--- test/skills/ProbabilityTest.java
+++ test/skills/ProbabilityTest.java
@@ -2,13 +2,17 @@ package skills;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class ProbabilityTest {
 
     @Test
     public void randomNum() {
 
         Probability.randomNum();
+    }
+
+    @Test
+    public void loop() {
+
+        Probability.loop();
     }
 }
