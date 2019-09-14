ea95aef407ff49fdf0ef166bd351f2f8809855e9
shadaab <shadaab@hiplind.com>
2019-09-14 19:03:24 +0530

    Resolved Prime Smallest Divisor

---
+++ projects/fundamentals/skills/PrimeSmallestDivisor.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PrimeSmallestDivisor {
+
+    private static int isPrime(long n){
+        int dividend = (int) n;
+        for (int i = 2; i < dividend; i++) {
+            if(dividend%i==0){
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter a Number");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(bufferedReader.readLine());
+        int psd = PrimeSmallestDivisor.isPrime(n);
+        System.out.println("The smallest divisor of "+ n +" is : "+ psd);
+    }
+}
